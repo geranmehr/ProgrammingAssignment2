ad096342fc27a2233a3383b79c125cad8a38fceb
geranmehr <53206035+geranmehr@users.noreply.github.com>
2023-05-22 11:29:57 +0330

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
-## Put comments here that give an overall description of what your
-## functions do
+library(caret)
+library(randomForest)
+library(varImp)
+regressor <- randomForest(OCSKGM ~ ., data = dat_sites, importance=TRUE)
+# fit the random forest with default parameter
+varImp(regressor)
+#  # get variable importance, based on mean decrease in accuracy
+varImp(regressor, conditional=TRUE)
+#   # conditional=True, adjusts for correlations between predictors
+varimpAUC(regressor) 
+#   # more robust towards class imbalance.
+#   
+#   
+#   
+gbmImp <- varImp(regressor, scale = TRUE)
+gbmImp
+plot(gbmImp)
 
-## Write a short comment describing this function
-
-makeCacheMatrix <- function(x = matrix()) {
-
-}
 
 
-## Write a short comment describing this function
+base.mod <- lm(OCSKGM ~ 2, data = dat_sites) # base intercept only 
+all.mod <- lm(OCSKGM ~ ., data = dat_sites) # full model with all predictors
+stepMod <- step(base.mod, scope = list(lower = base.mod, upper = all.mod), direction = "both", trace = 1, steps = 1000) # perform step-wise algorithm
+shortlistedVars <- names(unlist(stepMod[[1]])) # get the shortlisted variable.
+shortlistedVars <- shortlistedVars[!shortlistedVars %in% "(Intercept)"] # remove intercept
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+shortlistedVars
+
+
+ibrary(randomForest)
+set.seed(71)
+rf <-randomForest(OCSKGM ~ ., data = dat_sites, ntree=1000) 
+print(rf)
+rf
+floor(sqrt(ncol(dat_sites) - 1))
+mtry <- tuneRF(dat_sites[-1],dat_sites$OCSKGM, ntreeTry=1000,
+               stepFactor=1.5,improve=0.01, trace=TRUE, plot=TRUE)
+best.m <- mtry[mtry[, 2] == min(mtry[, 2]), 1]
+print(mtry)
+print(best.m)
+rf <-randomForest(OCSKGM~.,data=dat_sites, mtry=best.m, importance=TRUE,ntree=1000)
+print(rf)
+#Evaluate variable importance
+importance(rf)
+varImpPlot(rf)
+
+
+
+
