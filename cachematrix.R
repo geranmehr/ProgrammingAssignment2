4189ec917cf327c1e893d1890604a70f3d4b0290
geranmehr <53206035+geranmehr@users.noreply.github.com>
2023-05-22 11:37:38 +0330

    cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,48 +1,43 @@
-library(caret)
-library(randomForest)
-library(varImp)
-regressor <- randomForest(OCSKGM ~ ., data = dat_sites, importance=TRUE)
-# fit the random forest with default parameter
-varImp(regressor)
-#  # get variable importance, based on mean decrease in accuracy
-varImp(regressor, conditional=TRUE)
-#   # conditional=True, adjusts for correlations between predictors
-varimpAUC(regressor) 
-#   # more robust towards class imbalance.
-#   
-#   
-#   
-gbmImp <- varImp(regressor, scale = TRUE)
-gbmImp
-plot(gbmImp)
+# mohammadreza
 
 
+##
+## I simply set the input x as a matrix to sign it as my input
+## and then set the solved value "sol" as a null
+## then I have changed every reference to "mean" to "solve" 
+makeCacheMatrix <- function(x = matrix()) {
+        sol <- NULL
+        set <- function(y) {
+                x <<- y
+                sol <<- NULL
+        }
+        get <- function() x
+        setsolve <- function(solve) sol <<- solve
+        getsolve <- function() sol
+        list(set = set, get = get,
+             setsolve = setsolve,
+             getsolve = getsolve)
+}
 
-base.mod <- lm(OCSKGM ~ 2, data = dat_sites) # base intercept only 
-all.mod <- lm(OCSKGM ~ ., data = dat_sites) # full model with all predictors
-stepMod <- step(base.mod, scope = list(lower = base.mod, upper = all.mod), direction = "both", trace = 1, steps = 1000) # perform step-wise algorithm
-shortlistedVars <- names(unlist(stepMod[[1]])) # get the shortlisted variable.
-shortlistedVars <- shortlistedVars[!shortlistedVars %in% "(Intercept)"] # remove intercept
+## Same here, changed "mean" to "solve" and "m" to "sol"
+cacheSolve <- function(x, ...) {
+        sol <- x$getsolve()
+        if(!is.null(sol)) {
+                message("getting inversed matrix")
+                return(sol)
+        }
+        data <- x$get()
+        sol <- solve(data, ...)
+        x$setsolve(sol)
+        sol
+}
 
-shortlistedVars
+#check the function
 
+m <- matrix(rnorm(36),6,6)
+inv_mat <- makeCacheMatrix(m)
+cacheSolve(inv_mat)
 
-ibrary(randomForest)
-set.seed(71)
-rf <-randomForest(OCSKGM ~ ., data = dat_sites, ntree=1000) 
-print(rf)
-rf
-floor(sqrt(ncol(dat_sites) - 1))
-mtry <- tuneRF(dat_sites[-1],dat_sites$OCSKGM, ntreeTry=1000,
-               stepFactor=1.5,improve=0.01, trace=TRUE, plot=TRUE)
-best.m <- mtry[mtry[, 2] == min(mtry[, 2]), 1]
-print(mtry)
-print(best.m)
-rf <-randomForest(OCSKGM~.,data=dat_sites, mtry=best.m, importance=TRUE,ntree=1000)
-print(rf)
-#Evaluate variable importance
-importance(rf)
-varImpPlot(rf)
 
 
 
